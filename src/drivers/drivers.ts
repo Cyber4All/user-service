8a913cbf6c368566f3b938f0bc76b773db2376e6
Jewel7 <volleyballjewel@gmail.com>
2018-02-15 12:00:32 -0500

    Add User to driver .ts file

--- src/drivers/drivers.ts
+++ src/drivers/drivers.ts
@@ -2,6 +2,7 @@ import MongoDriver from './MongoDriver';
 import RouteHandler from './RouteHandler';
 import RouteResponder from './RouteResponder';
 import * as TokenManager from './TokenManager';
-import { UserResponseFactory } from './UserResponseFactory'
+import { UserResponseFactory } from './UserResponseFactory';
+import { User } from './UserDriver';
 
-export { MongoDriver, RouteHandler, RouteResponder, TokenManager, UserResponseFactory }
+export { MongoDriver, RouteHandler, RouteResponder, TokenManager, UserResponseFactory, User }
