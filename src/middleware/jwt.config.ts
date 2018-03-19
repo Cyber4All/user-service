6bc18fabcdc483bb70e43f945f683bb709414080
Gustavus Shaw II <gshaw1997@gmail.com>
2018-03-19 13:33:53 -0400

    Add validate route to jwt unless

--- src/middleware/jwt.config.ts
+++ src/middleware/jwt.config.ts
@@ -8,18 +8,10 @@ dotenv.config();
  * @author Gustavus Shaw II
  */
 export const enforceTokenAccess = jwt({
-  secret: process.env.KEY,
-  issuer: process.env.ISSUER,
-  getToken: req => {
-    return req.cookies.presence;
-  }
-}).unless({
-  // Routes that don't require authorization
-  path: [
-    '/',
-    '/validate-captcha',
-    { url: '/users', methods: ['POST', 'GET'] }, // register & search
-    '/users/ota-codes', // all ota-code routes do their own verifcation outsides of JWT
-    { url: '/users/tokens', methods: ['POST'] }
-  ] // login
-});
+         secret: process.env.KEY,
+         issuer: process.env.ISSUER,
+         getToken: req => {
+           return req.cookies.presence;
+         }
+       }).unless({ // Routes that don't require authorization
+         path: ['/', '/validate-captcha', { url: '/users', methods: ['POST', 'GET'] }, '/users/ota-codes', { url: '/users/tokens', methods: ['POST'] }, 'validate-captcha'] }); // register & search // all ota-code routes do their own verifcation outsides of JWT // login
