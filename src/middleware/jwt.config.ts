9dfc6d3ff21f1556f82d15b8d7dab3eea7e84bc9
Gustavus Shaw II <gshaw1997@gmail.com>
2018-04-26 12:07:39 -0400

    chore: lint clean up

--- src/middleware/jwt.config.ts
+++ src/middleware/jwt.config.ts
@@ -8,10 +8,20 @@ dotenv.config();
  * @author Gustavus Shaw II
  */
 export const enforceTokenAccess = jwt({
-         secret: process.env.KEY,
-         issuer: process.env.ISSUER,
-         getToken: req => {
-           return req.cookies.presence;
-         }
-       }).unless({ // Routes that don't require authorization
-         path: ['/', '/validate-captcha', { url: '/users', methods: ['POST', 'GET'] }, '/users/ota-codes', { url: '/users/tokens', methods: ['POST'] }, 'validate-captcha', '/users/identifiers/active'] }); // register & search // all ota-code routes do their own verifcation outsides of JWT // login
+  secret: process.env.KEY,
+  issuer: process.env.ISSUER,
+  getToken: req => {
+    return req.cookies.presence;
+  }
+}).unless({
+  // Routes that don't require authorization
+  path: [
+    '/',
+    '/validate-captcha',
+    { url: '/users', methods: ['POST', 'GET'] },
+    '/users/ota-codes',
+    { url: '/users/tokens', methods: ['POST'] },
+    'validate-captcha',
+    '/users/identifiers/active'
+  ]
+}); // register & search // all ota-code routes do their own verifcation outsides of JWT // login
