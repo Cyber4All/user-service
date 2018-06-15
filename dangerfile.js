ecc62a0c379780b8dfd9ce4008ad367d63b74286
Sean Donnelly <sdonnelly3000@gmail.com>
2018-06-15 15:43:52 -0400

    Add rule to dangerfile
    PRs will now be checked such that all new files include tests.

--- dangerfile.js
+++ dangerfile.js
@@ -1,4 +1,26 @@
-import {message, danger} from "danger"
+import {warn, danger} from "danger"
 
-const modifiedMD = danger.git.modified_files.join("- ")
-message("Changed Files in this PR: \n - " + modifiedMD)
+const newFiles = danger.git.created_files;
+
+const EXTENSION = '.js';
+const filesWithoutTests = [];
+
+// Rule 1: New files must come with tests.
+if (newFiles.length > 0) {
+  for (file of newFiles) {
+    // Only check non-spec files
+    if (!file.endsWith(`spec${EXTENSION}`)) {
+      // Get the filename up to the extension
+      // This guarentees spec files are committed next to the relevant production code
+      const filenameWithoutExtension = file.split(EXTENSION)[0];
+      expectedSpecFile = `${filenameWithoutExtension}.spec${EXTENSION}`;
+
+      if (!newFiles.includes(expectedSpecFile)) {
+        filesWithoutTests.push(file);
+      }
+    }
+  }
+}
+warn(
+  `The following new files do not have associated spec files included: ${filesWithoutTests.toString()}.`
+);
