08651cfbbb40455d4d60f34afd2163d93c9aaa33
Gustavus Shaw II <gshaw1997@gmail.com>
2018-11-12 08:39:18 -0500

    :sparkles: Add route for stats

---
+++ src/UserStats/UserStatsRouteHandler.ts
@@ -0,0 +1,27 @@
+import * as UserStatsInteractor from './UserStatsInteractor';
+import { Request, Response, Router } from 'express';
+import { DataStore } from '../interfaces/DataStore';
+
+/**
+ * Initializes an express router with endpoints to get stats for a User
+ */
+export function initialize({ dataStore }: { dataStore: DataStore }) {
+  const router: Router = Router();
+  const getStats = async (req: Request, res: Response) => {
+    try {
+      const query = req.query;
+      const stats = await UserStatsInteractor.getStats({
+        dataStore,
+        query
+      });
+
+      res.status(200).send(stats);
+    } catch (e) {
+      console.error(e);
+      res.status(500).send(e);
+    }
+  };
+
+  router.route('').get(getStats);
+  return router;
+}
