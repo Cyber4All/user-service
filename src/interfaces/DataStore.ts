8bc7673790fa9f4620ef24e794ea155dfaf8fe8c
Gustavus Shaw II <gshaw1997@gmail.com>
2019-01-08 07:36:30 -0500

    :recycle: Update types in Datastore

--- src/interfaces/DataStore.ts
+++ src/interfaces/DataStore.ts
@@ -1,19 +1,19 @@
-import { User } from '@cyber4all/clark-entity';
 import { OTACode } from '../drivers/OTACodeManager';
 import { UserQuery } from './Query';
+import { AuthUser } from '../types/auth-user';
 
 export interface DataStore {
   connect(dbURI: string): Promise<void>;
   disconnect(): void;
   identifierInUse(username: string): Promise<boolean>;
-  insertUser(user: User): Promise<string>;
+  insertUser(user: AuthUser): Promise<string>;
   findUser(username: string): Promise<string>;
-  loadUser(id: string): Promise<User>;
-  editUser(id: string, user: {}): Promise<User>;
+  loadUser(id: string): Promise<AuthUser>;
+  editUser(id: string, user: { [index: string]: any }): Promise<AuthUser>;
   deleteUser(id: string): Promise<void>;
   insertOTACode(otaCode: OTACode): Promise<void>;
   findOTACode(otaCode: string): Promise<string>;
   deleteOTACode(id: string): Promise<void>;
-  searchUsers(query: UserQuery): Promise<{ users: User[]; total: number }>;
+  searchUsers(query: UserQuery): Promise<{ users: AuthUser[]; total: number }>;
   findOrganizations(query: string): Promise<any[]>;
 }
