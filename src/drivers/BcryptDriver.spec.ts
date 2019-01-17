1293bb8cbda651757079e274da31dfe881979306
Sean Donnelly <sdonnelly3000@gmail.com>
2019-01-17 13:35:42 -0500

    :memo: Document Bcrypt Test

--- src/drivers/BcryptDriver.spec.ts
+++ src/drivers/BcryptDriver.spec.ts
@@ -1,5 +1,12 @@
 import { BcryptDriver } from "./drivers";
 
+/**
+ * This test is here to verify that the library is working on each build.
+ * Normally, we would not test that a library works properly, but we have
+ * had issues with bcrypt not installing properly and crashing a deployment.
+ * 
+ * @author Sean Donnelly <sdonne5@students.towson.edu>
+ */
 describe('BcryptDriver', () => {
   let bcrypt: BcryptDriver;
   beforeEach(() => {
