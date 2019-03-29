d6f6e4d85d70b0d7b45a7eb8dd3a3d7cc639b669
Gustavus Shaw II <gshaw1997@gmail.com>
2019-03-29 11:02:18 -0400

    :recycle: Refactor reportError

--- src/shared/SentryConnector.ts
+++ src/shared/SentryConnector.ts
@@ -3,16 +3,16 @@ import * as express from 'express';
 
 const environment = process.env.NODE_ENV;
 
-let handleErrorReporting: (e: Error) => void;
+export let reportError: (e: Error) => void;
 
 switch (environment) {
   case 'development':
-    handleErrorReporting = console.error;
+    reportError = console.error;
     break;
   case 'production':
     // Note: The DSN is not needed for local development since we only want to report production errors
     Sentry.init({ dsn: process.env.SENTRY_DSN });
-    handleErrorReporting = Sentry.captureException;
+    reportError = Sentry.captureException;
     break;
   default:
     break;
@@ -20,6 +20,3 @@ switch (environment) {
 
 export const sentryRequestHandler = Sentry.Handlers.requestHandler() as express.RequestHandler;
 export const sentryErrorHandler = Sentry.Handlers.errorHandler() as express.ErrorRequestHandler;
-export const reportError = (error: Error) => {
-  handleErrorReporting(error);
-};
