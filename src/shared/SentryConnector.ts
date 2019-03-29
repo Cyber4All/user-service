fe10a78c402184f5ce43fcaefe9b093cdc0b00c0
Gustavus Shaw II <gshaw1997@gmail.com>
2019-03-29 11:04:00 -0400

    :recycle: Only export consts

--- src/shared/SentryConnector.ts
+++ src/shared/SentryConnector.ts
@@ -3,16 +3,16 @@ import * as express from 'express';
 
 const environment = process.env.NODE_ENV;
 
-export let reportError: (e: Error) => void;
+let _reportError: (e: Error) => void;
 
 switch (environment) {
   case 'development':
-    reportError = console.error;
+    _reportError = console.error;
     break;
   case 'production':
     // Note: The DSN is not needed for local development since we only want to report production errors
     Sentry.init({ dsn: process.env.SENTRY_DSN });
-    reportError = Sentry.captureException;
+    _reportError = Sentry.captureException;
     break;
   default:
     break;
@@ -20,3 +20,4 @@ switch (environment) {
 
 export const sentryRequestHandler = Sentry.Handlers.requestHandler() as express.RequestHandler;
 export const sentryErrorHandler = Sentry.Handlers.errorHandler() as express.ErrorRequestHandler;
+export const reportError = _reportError;
