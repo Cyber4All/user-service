4b2f2a386b6d421905f2d36a5fd8cc92f3f6279e
Gustavus Shaw II <gshaw1997@gmail.com>
2019-04-19 06:59:38 -0400

    Update user token stubs

--- src/collection-role/MocksObjects.ts
+++ src/collection-role/MocksObjects.ts
@@ -5,27 +5,30 @@ export const COLLECTION_ROLE_MOCK_OBJECTS = {
   ROLE_CURATOR: 'curator',
   ROLE_REVIEWER: 'reviewer',
   USER_TOKEN_ADMIN: {
+    id: '123',
     username: 'admin',
     name: 'unit test',
     email: 'test@admin.com',
     organization: 'admin',
     emailVerified: false,
-    accessGroups: ['admin'],
+    accessGroups: ['admin']
   },
   USER_TOKEN_CURATOR_C5: {
+    id: '456',
     username: 'curator',
     name: 'unit test',
     email: 'test@curator.com',
     organization: 'curator',
     emailVerified: false,
-    accessGroups: ['curator@c5'],
+    accessGroups: ['curator@c5']
   },
   USER_TOKEN_CURATOR_NCCP: {
+    id: '789',
     username: 'curator',
     name: 'unit test',
     email: 'test@curator.com',
     organization: 'curator',
     emailVerified: false,
-    accessGroups: ['curator@nccp'],
-  },
+    accessGroups: ['curator@nccp']
+  }
 };
