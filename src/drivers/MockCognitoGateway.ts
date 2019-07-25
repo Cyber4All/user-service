63df65342858f9e7d3f450b021c8bd76df063d52
Gustavus Shaw II <gshaw1997@gmail.com>
2019-07-25 12:08:08 -0400

    :recycle: Update CognitoIdentityManager params

--- src/drivers/MockCognitoGateway.ts
+++ src/drivers/MockCognitoGateway.ts
@@ -1,8 +1,8 @@
 import { CognitoGateway } from '../interactors/AuthenticationInteractor';
-import { OpenIdToken, UserToken } from '../CognitoIdentityManager/typings';
+import { OpenIdToken } from '../CognitoIdentityManager/typings';
 
 export class MockCognitoGateway implements CognitoGateway {
-  getOpenIdToken(_: { requester: UserToken }): Promise<OpenIdToken> {
+  getOpenIdToken(_: { username: string, isAdminOrEditor?:boolean }): Promise<OpenIdToken> {
     const token: OpenIdToken = {
       IdentityId: '123',
       Token: '456'
