68ebe0b7765d447edde31dd19040cb66cbfc0f85
Nick Winner <design@winnerdigital.net>
2019-08-29 11:45:05 -0400

    :white_check_mark: Update AdminUser.interactor tests from chai to jest

--- src/interactors/AdminUser.interactor.spec.ts
+++ src/interactors/AdminUser.interactor.spec.ts
@@ -1,42 +1,26 @@
 import { AdminUserInteractor } from '../interactors/AdminUserInteractor';
 import MockDriver from '../drivers/MockDriver';
 import { MOCK_OBJECTS } from '../tests/mocks';
-import { expect } from 'chai';
 const driver = new MockDriver(); // DataStore
 
 describe('fetchUsers', () => {
-  it('should return an array of users', done => {
-    return AdminUserInteractor.fetchUsers(driver, MOCK_OBJECTS.USERNAME_QUERY)
-      .then(val => {
-        expect(val).to.be.an('object');
-        done();
-      })
-      .catch(error => {
-        expect.fail();
-        done();
-      });
+  it('should return an array of users', async () => {
+    const results = await AdminUserInteractor.fetchUsers(driver, MOCK_OBJECTS.USERNAME_QUERY)
+    expect(results.total).toBeGreaterThan(0)
+    expect(results.users).toBeInstanceOf(Array);
   });
-  it('password should be undefined on user object when returned!', done => {
-    return AdminUserInteractor.fetchUsers(driver, MOCK_OBJECTS.USERNAME_QUERY)
-      .then(val => {
-        expect(val.users[0]['_password'], 'user not returned!').to.be.undefined;
-        done();
-      })
-      .catch(error => {
-        expect.fail();
-        done();
-      });
+
+  it('password should be undefined on user object when returned', async () => {
+    const result = await AdminUserInteractor.fetchUsers(driver, MOCK_OBJECTS.USERNAME_QUERY);
+    expect(
+      // @ts-ignore
+      result.users.filter(user => typeof user.password !== 'undefined' && user.password !== '').length
+    ).toBe(0);
   });
-  it('should return all users. empty query', done => {
-    return AdminUserInteractor.fetchUsers(driver, MOCK_OBJECTS.EMPTY_USERNAME_QUERY)
-      .then(val => {
-        expect(val, 'users is not an array!').to.be.a('object');
-        done();
-      })
-      .catch(error => {
-        expect.fail();
-        console.log(error);
-        done();
-      });
+
+  it('should return all users. empty query', async () => {
+    await expect(
+      AdminUserInteractor.fetchUsers(driver, MOCK_OBJECTS.EMPTY_USERNAME_QUERY)
+    ).resolves.toBeDefined();
   });
 });
