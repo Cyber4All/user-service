c8927a5a4d6845ee67574faffc547be7a64f747c
Emily Vogel <eav990@gmail.com>
2021-03-08 10:34:19 -0500

    Remove request/request-promise

--- src/gateways/file-access-identities/HttpFileAccessIdentityGateway.ts
+++ src/gateways/file-access-identities/HttpFileAccessIdentityGateway.ts
@@ -1,12 +1,12 @@
 import { FILE_ACCESS_IDENTITY_ROUTES, LEARNING_OBJECT_ROUTE  } from './routes';
 import { generateServiceToken } from '../../drivers/TokenManager';
-import * as request from 'request-promise';
+
+const fetch = require('node-fetch');
 
 // FIXME: Create interface for this Gateway
 export class HttpFileAccessIdentityGateway {
     private static options = {
       uri: '',
-      json: true,
       headers: {
         Authorization: 'Bearer',
       },
@@ -21,8 +21,8 @@ export class HttpFileAccessIdentityGateway {
       }
       options.uri = LEARNING_OBJECT_ROUTE.getCollection();
       options.headers.Authorization = `Bearer ${generateServiceToken()}`;
-      return await request(options)
-
+      delete options.body;
+      return await (await fetch(options.uri, options)).json();
     }
 
     static async createFileAccessIdentity({
@@ -35,14 +35,11 @@ export class HttpFileAccessIdentityGateway {
       const options = { 
         ...this.options,
         method: 'POST',
-        body: {
-          fileAccessID: fileAccessIdentity,
-        }
+        body: JSON.stringify({ fileAccessID: fileAccessIdentity })
       }
       options.uri = FILE_ACCESS_IDENTITY_ROUTES.createFileAccessIdentity(username);
       options.headers.Authorization = `Bearer ${generateServiceToken()}`;
-      const res = await request(options);
-      return res;
+      return await (await fetch(options.uri, options)).json();
     }
 
     static async updateFileAccessIdentity({
@@ -53,10 +50,9 @@ export class HttpFileAccessIdentityGateway {
         fileAccessIdentity: string,
     }): Promise<string> {
         const options = { ...this.options };
-        options.body = { fileAccessID: fileAccessIdentity };
+        options.body = JSON.stringify({ fileAccessID: fileAccessIdentity });
         options.uri = FILE_ACCESS_IDENTITY_ROUTES.updateFileAccessIdentity(username);
         options.headers.Authorization = `Bearer ${generateServiceToken()}`;
-        const res = await request(options);
-        return res;
+        return await (await fetch(options.uri, options)).json();
     }
   }
